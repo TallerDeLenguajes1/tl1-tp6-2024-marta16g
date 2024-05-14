1a0b11dbbf270b39bda8cdd80128b23d883bc1e9
Clari002 <claridiaz02@gmail.com>
2024-05-14 13:14:02 -0300

    Primer punto hecho, quizás lo mejore

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int a;
+int b;
+a = 10;
+b = a;
+
+Console.WriteLine("El valor de a es " + a);
+Console.WriteLine("El valor de b es " + b);
+
+//Punto 1
+
+Console.WriteLine("Iniciando punto 1");
+
+int numero;
+int invertido;
+
+Console.WriteLine("Ingrese un número");
+
+if (int.TryParse(Console.ReadLine(), out numero))
+{
+    if (numero > 0)
+    {
+        invertido = invertirNumero(numero);
+        Console.WriteLine("El numero invertido de " + numero + " es " + invertido);
+    }
+    else
+    {
+        Console.WriteLine("El número ingresado debe ser mayor a 0 sorry bye");
+    }
+}
+else
+{
+    Console.WriteLine("Lo ingresado no es un numero chau");
+};
+
+
+static int invertirNumero(int num)
+{
+
+    int centenas, decenas, unidades, aux;
+    if (num > 9)
+    {
+        if (!(num > 99))
+        {
+            decenas = num / 10;
+            unidades = num % 10;
+
+            aux = (unidades * 10) + decenas;
+        }
+        else
+        {
+            centenas = num / 100;
+            num %= 100;
+
+            decenas = num / 10;
+            unidades = num % 10;
+
+            aux = (unidades * 100) + (decenas * 10) + centenas;
+
+        }
+    }
+    else
+    {
+        aux = num;
+    };
+
+    return aux;
+};
